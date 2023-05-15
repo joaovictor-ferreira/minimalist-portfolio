7e35a440948ccd8216e4f3d928672f2e487ab3d8
João Victor Dos Santos Ferreira <0xjoao@Joaos-MacBook-Pro.local>
2023-05-15 16:17:39 -0300

    adicionando o dark mode

--- js/index.js
+++ js/index.js
@@ -1,15 +1,94 @@
 const navToggle = document.getElementById("toggle");
 const menu = document.querySelector(".menu__ul");
+const menuContainer = document.getElementById("menu");
 const body = document.body;
+const home = document.getElementById("home-section");
+const aboutMe = document.getElementById("about-me-section");
+const content = document.querySelectorAll(".container");
+//seleciona todas as seções que receberão a animação do estilo "fade-down"
+const fadeDownItems = document.querySelectorAll(".fade-down > *");
+
+const darkModeBtn = document.querySelector(".dark-mode-btn");
+const darkModeItems = document.querySelectorAll(".dark-item")
+
 
 navToggle.addEventListener("click", () => {
-  if (menu.style.display === "none") {
-    menu.style.display = "block";
-    menu.style.transform = "translateX(0)";
-  } else {
-    menu.style.transform = "translateX(100%)";
-    setTimeout(() => {
-      menu.style.display = "none";
-    }, 300); 
+    if (menu.style.display === "none") {
+      menu.style.display = "block";
+      menu.style.transform = "translateX(0)";
+      navToggle.classList.remove("fa-bars")
+      navToggle.classList.add("fa-x")
+    } else {
+      menu.style.transform = "translateX(100%)";
+      setTimeout(() => {
+        menu.style.display = "none";
+      }, 300); 
+      navToggle.classList.remove("fa-x")
+      navToggle.classList.add("fa-bars")
+    }
+  });
+  
+darkModeBtn.addEventListener("click", () => {
+  toggleDarkMode()
+})
+
+function toggleDarkMode() {
+    const isDarkMode = body.classList.contains('dark-mode');
+
+    if (isDarkMode) {
+    // Modo escuro ativo
+    body.classList.remove('dark-mode');
+    menuContainer.classList.remove('dark-mode');
+
+    aboutMe.style.background = 'transparent';
+    home.style.background = 'transparent';
+    darkModeBtn.classList.remove("fa-lightbulb");
+    darkModeBtn.classList.add("fa-moon");
+    darkModeItems.forEach(element => {
+        element.style.color = '#1d1d1d';
+    });
+    } else {
+    // Modo claro ativo
+    body.classList.add('dark-mode');
+    menuContainer.classList.add('dark-mode');
+    menuContainer.style.background = '#000000';
+    aboutMe.style.background = 'transparent';
+    home.style.background = 'transparent';
+    darkModeBtn.classList.remove("fa-moon");
+    darkModeBtn.classList.add("fa-lightbulb");
+    darkModeItems.forEach(element => {
+        element.style.color = '#e6edf3';
+    });
+    }
+}
+
+
+window.addEventListener("load",() => {
+    fadeDownItems.forEach(element => {
+        element.style.display= "none";
+    })
+})
+
+// Função para verificar se a seção está visível no viewport
+function isSectionVisible(section) {
+    const rect = section.getBoundingClientRect();
+    const windowHeight = window.innerHeight || document.documentElement.clientHeight;
+    return rect.top <= windowHeight * 0.8;
   }
+  
+  // Adicione um evento de rolagem para verificar a visibilidade das seções
+window.addEventListener("scroll", () => {
+    fadeDownItems.forEach((element) => {
+        if (isSectionVisible(element)) {
+        fadeDown(element);
+    }
 });
+});
+  
+
+//função que invoca o comportamento de fadeDown adicionando as classes 
+function fadeDown(item) {
+    item.classList.add("animate__animated");
+    item.classList.add("animate__fadeInDown");
+    item.style.display = "flex";
+}
