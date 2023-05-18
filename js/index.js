774057250b359b86ad9a54edd4a3523cdd57deb4
João Victor Dos Santos Ferreira <0xjoao@Joaos-MacBook-Pro.local>
2023-05-17 23:22:56 -0300

    adicionando efeito de hover ao dark mode com javascript

--- js/index.js
+++ js/index.js
@@ -1,6 +1,8 @@
 const navToggle = document.getElementById("toggle");
 const menu = document.querySelector(".menu__ul");
 const menuContainer = document.getElementById("menu");
+//seleciona os filhos do menu
+const menuItems = menu.querySelectorAll("*")
 const body = document.body;
 const home = document.getElementById("home-section");
 const aboutMe = document.getElementById("about-me-section");
@@ -38,29 +40,39 @@ function toggleDarkMode() {
     const isDarkMode = body.classList.contains('dark-mode');
 
     if (isDarkMode) {
-    // Modo escuro ativo
-    body.classList.remove('dark-mode');
-    menuContainer.classList.remove('dark-mode');
-    menuContainer.style.background = '#ffffff'
-    aboutMe.style.background = 'transparent';
-    home.style.backgroundImage = 'url(/assets/waves-bg.svg)';
-    darkModeBtn.classList.remove("fa-lightbulb");
-    darkModeBtn.classList.add("fa-moon");
-    darkModeItems.forEach(element => {
-        element.style.color = '#1d1d1d';
-    });
-    } else {
-    // Modo claro ativo
-    body.classList.add('dark-mode');
-    menuContainer.classList.add('dark-mode');
-    menuContainer.style.background = '#000000';
-    aboutMe.style.background = 'transparent';
-    home.style.background = 'transparent';
-    darkModeBtn.classList.remove("fa-moon");
-    darkModeBtn.classList.add("fa-lightbulb");
-    darkModeItems.forEach(element => {
-        element.style.color = '#e6edf3';
-    });
+      // Modo escuro ativo
+      body.classList.remove('dark-mode');
+      menuContainer.classList.remove('dark-mode');
+      menuContainer.style.background = '#ffffff'
+      aboutMe.style.backgroundImage = "linear-gradient(to bottom, #eaeaea, #eeeeee, #f2f2f2, #f6f6f6, #fafafa, #fcfcfc, #fdfdfd, #ffffff, #ffffff, #ffffff, #ffffff, #ffffff)";
+      home.style.backgroundImage = 'url(/assets/waves-bg.svg)';
+      darkModeBtn.classList.remove("fa-lightbulb");
+      darkModeBtn.classList.add("fa-moon");
+      darkModeItems.forEach(element => {
+          element.style.color = '#1d1d1d';
+      });
+      //evento para aplicar efeito hover durante darkmode via javascript
+      menuItems.forEach(element => {
+        element.addEventListener("mouseenter", () => element.style.color = "var(--primary-color)");
+        element.addEventListener("mouseleave", () => element.style.color = "#1d1d1d");
+      })
+    }
+      else {
+        // Modo claro ativo
+        body.classList.add('dark-mode');
+        menuContainer.classList.add('dark-mode');
+        menuContainer.style.background = '#000000';
+        aboutMe.style.background = 'transparent';
+        home.style.background = 'transparent';
+        darkModeBtn.classList.remove("fa-moon");
+        darkModeBtn.classList.add("fa-lightbulb");
+        darkModeItems.forEach(element => {
+            element.style.color = '#e6edf3';
+        });
+        menuItems.forEach(element => {
+          element.addEventListener("mouseenter", () => element.style.color = "var(--primary-color)");
+          element.addEventListener("mouseleave", () => element.style.color = "#e6edf3");
+        })
     }
 }
 
